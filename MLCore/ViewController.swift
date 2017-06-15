8a4f6405fda0575012bfcf4c14cbdf65198865dd
Quintin Smith <quintin@getweave.com>
2017-06-14 22:12:58 -0600

    Initial Commit

---
+++ MLCore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MLCore
+//
+//  Created by Quintin Smith on 6/14/17.
+//  Copyright © 2017 wasatch code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
